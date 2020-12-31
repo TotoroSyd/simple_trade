d056422c8378f749f4c6b082b3ffe6596f539cee
TotoroSyd <phoebe.ngsyd@gmail.com>
2020-12-31 00:59:53 +0000

    Refactor

--- application.py
+++ application.py
@@ -7,7 +7,6 @@ import json
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
